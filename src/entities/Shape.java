9bca407430ba49205f8e32b3aec13a8c58c67ca7
Caio André <caio_andre15@hotmail.com>
2020-08-04 22:59:04 -0300

    Abstract shape class created in entities package

---
+++ src/entities/Shape.java
@@ -0,0 +1,29 @@
+package entities;
+
+import entities.enums.Color;
+
+public abstract class Shape {
+
+	// Attributes
+	private Color color;
+
+	// Builders
+	public Shape() {
+	}
+
+	public Shape(Color color) {
+		this.color = color;
+	}
+
+	// Accessor Methods
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public abstract Double area();
+
+}
